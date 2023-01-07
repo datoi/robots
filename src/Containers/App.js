e8312407e480c786636ba2d9f69f47f16860a287
DaViT <43728693+datoi@users.noreply.github.com>
2023-01-07 20:32:31 +0400

    Update App.js

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,6 +1,7 @@
 import {Component} from "react";
 import CardList from "../Components/CardList";
 import './App.css'
+import ErrorBoundry from "../Components/ErrorBoundry";
 import SearchBox from "../Components/SearchBox";
 import Scroll from "../Components/Scroll";
 
